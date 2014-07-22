cc7cac397a7497b04c69b15ba85980d396093e98
Neil Nee <neilnee324@gmail.com>
2014-07-22 22:16:28 +0800

    完成对忽略目录表、优先目录表的配置

--- disk_scan/disk_scan_tool.cpp
+++ disk_scan/disk_scan_tool.cpp
@@ -1,15 +1,68 @@
 #include "stdafx.h"
 #include "disk_scan_tool.h"
 
+#include <algorithm>
+
 using namespace xl_ds_api;
 
 CScanner::CScanner()
 {
-    m_ScanDirs = 0;
+	Init();
+	ScanBaseDir();
 }
 
 CScanner::~CScanner()
 {
+	UnInit();
+}
+
+void CScanner::Init()
+{
+	m_ScanDirs = 0;
+	m_BaseDirs.clear();
+	m_PriorityDirs.clear();
+	m_IgnoreDirs.clear();
+	m_ScanTargetCallback = NULL;
+
+	INT ignores[] = IGNORE_DIRS;
+	INT prioritys[] = PRIORITY_DIRS;
+	TCHAR szPath[MAX_PATH];
+
+	size_t size  = sizeof(prioritys)/sizeof(prioritys[0]);
+	for (INT i=0; i<size; i++) {
+		SHGetFolderPath(NULL, prioritys[i], NULL, 0, szPath);
+		std::wstring directory = szPath;
+		PushBackDir(m_PriorityDirs, directory);
+	}
+	size  = sizeof(ignores)/sizeof(ignores[0]);
+	for (INT i=0; i<size; i++) {
+		SHGetFolderPath(NULL, ignores[i], NULL, 0, szPath);
+		std::wstring directory = szPath;
+		PushBackDir(m_IgnoreDirs, directory);
+	}
+}
+
+void CScanner::UnInit()
+{
+	m_ScanDirs = 0;
+	m_PriorityDirs.clear();
+	m_IgnoreDirs.clear();
+	m_BaseDirs.clear();
+	m_ScanTargetCallback = NULL;
+}
+
+void CScanner::PushBackDir(std::vector<std::wstring> &dirList, std::wstring &directory) 
+{
+	if (directory.rfind(L"\\") != directory.length() -1) {
+		directory.append(L"\\");
+	}
+	if (std::find(dirList.begin(), dirList.end(), directory) == dirList.end()) {
+		dirList.push_back(directory);
+	}
+}
+
+void CScanner::SetScanTargetCallback(ScanTargetCallback callback) {
+	m_ScanTargetCallback = callback;
 }
 
 void CScanner::ScanBaseDir()
@@ -41,14 +94,15 @@ void CScanner::ScanBaseDir()
 					//需要过滤掉.和..
                     if ((fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
                         && !(fileData.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM)) {
-                        std::wstring strFileName = fileData.cFileName;
-                        if (strFileName != L"Windows"
-                            && strFileName != L"Program Files"
-                            && strFileName != L"Program Files (x86)"
-                            && strFileName != L"ProgramData") {
-                                std::wstring targetDir = szDrive+strFileName;
-                                m_BaseDirs.push_back(targetDir);
-                        }
+                        std::wstring fileName = fileData.cFileName;
+						std::wstring directory = szDrive+fileName;
+						if (fileName == L"Windows"
+							|| fileName == L"Program Files"
+							|| fileName == L"ProgramData"
+							|| fileName == L"Program Files (x86)") {
+								PushBackDir(m_IgnoreDirs, directory);
+						}
+						m_BaseDirs.push_back(directory);
                     }
                     finish = !FindNextFile(handle, &fileData);
                 } while (!finish);
@@ -59,24 +113,12 @@ void CScanner::ScanBaseDir()
     }
 }
 
-void CScanner::ScanPriorityDir()
-{
-    m_PriorityDirs.clear();
-    INT prioritys[] = PRIORITY_DIRS;
-    TCHAR szPath[MAX_PATH];
-    size_t size  = sizeof(prioritys)/sizeof(prioritys[0]);
-    for (INT i=0; i<size; i++) {
-        SHGetFolderPath(NULL, prioritys[i], NULL, 0, szPath);
-        std::wstring directory = szPath;
-        m_PriorityDirs.push_back(directory);
-    }
-}
-
-void CScanner::ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std::wstring> &targetDir, ScanTargetCallback callback)
+void CScanner::ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std::wstring> &targetDir, BOOL priority)
 {
 	if (baseDir == NULL || baseDir->size() <= 0) {
 		return;
 	}
+
 	std::vector<std::wstring>::iterator iter;
 	std::vector<std::wstring> searchDir;
 	for (iter = baseDir->begin(); iter != baseDir->end(); iter++) {
@@ -89,10 +131,20 @@ void CScanner::ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std
             dirStr.append(L"\\");
         }
 
-        //开始扫描directory目录
-        if (callback != NULL) {
-            callback(m_ScanDirs, dirStr);
-        }
+		// 跳过忽略文件夹
+		if (std::find(m_IgnoreDirs.begin(), m_IgnoreDirs.end(), dirStr) != m_IgnoreDirs.end()) {
+			continue;
+		}
+
+		// 如果是非优先扫描，则跳过优先扫描文件夹
+		if (!priority && std::find(m_PriorityDirs.begin(), m_PriorityDirs.end(), dirStr) != m_PriorityDirs.end()) {
+			continue;
+		}
+
+		//开始扫描directory目录，并回调进度
+		if (m_ScanTargetCallback != NULL) {
+			m_ScanTargetCallback(m_ScanDirs, dirStr);
+		}
         LPCTSTR directory = dirStr.c_str();
 		SetCurrentDirectory(directory);
 
@@ -115,6 +167,7 @@ void CScanner::ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std
                             std::wstring::size_type suffixPos = strFileName.rfind(L".");
                             if (suffixPos != std::wstring::npos) {
                                 std::wstring suffix = strFileName.substr(suffixPos);
+								std::transform(suffix.begin(), suffix.end(), suffix.begin(), tolower);
                                 if (IMG_SUFFIX->find(suffix) == std::wstring::npos) {
                                     otherCount++;
                                 } else {
@@ -122,7 +175,7 @@ void CScanner::ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std
                                 }
                             }
                             // 目标文件夹的内层判定判定规则，遍历中判定
-                            if (targetCount > 10 && otherCount == 0) {
+                            if (targetCount > 50 && otherCount == 0) {
                                 found = TRUE;
                             }
                         }
@@ -131,13 +184,13 @@ void CScanner::ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std
 				finish = !FindNextFile(handle, &fileData);
 			} while (!finish);
             // 目标文件夹的外层判定判定规则，遍历完后判定
-            if(found) {
+            if(found || (targetCount > 20 && otherCount == 0)) {
                 targetDir.push_back(directory);
             }
 		}
         m_ScanDirs++;
 	}
     if (searchDir.size() > 0) {
-        ScanTargetDir(&searchDir, targetDir, callback);
+        ScanTargetDir(&searchDir, targetDir, priority);
     }
 }
--- disk_scan/disk_scan_tool.h
+++ disk_scan/disk_scan_tool.h
@@ -6,8 +6,8 @@
 #include <string>
 #include <ShlObj.h>
 
-#define PRIORITY_DIRS {CSIDL_DESKTOP, CSIDL_MYDOCUMENTS, CSIDL_MYPICTURES, CSIDL_COMMON_PICTURES, CSIDL_COMMON_DOCUMENTS, CSIDL_COMMON_DESKTOPDIRECTORY};
-#define INGORE_DIRS {}
+#define PRIORITY_DIRS {CSIDL_DESKTOP, CSIDL_MYDOCUMENTS, CSIDL_MYPICTURES, CSIDL_COMMON_PICTURES, CSIDL_COMMON_DOCUMENTS, CSIDL_COMMON_DESKTOPDIRECTORY}
+#define IGNORE_DIRS {CSIDL_WINDOWS, CSIDL_PROGRAM_FILES, CSIDL_TEMPLATES, CSIDL_PROGRAM_FILESX86, CSIDL_APPDATA, CSIDL_COMMON_APPDATA, CSIDL_COOKIES, CSIDL_INTERNET_CACHE, CSIDL_LOCAL_APPDATA}
 
 static const DWORD BUF_SIZE  = 512;
 static const std::wstring IMG_SUFFIX[] = {
@@ -24,31 +24,47 @@ namespace xl_ds_api
         virtual ~CScanner();
 
 		/**
-		 * 扫描出本地各磁盘的一级目录（根据规则过滤掉不在扫描范围内的目录）
-		 *
-		 * @param baseDirs : out, 扫描到的一级目录列表
+		 * 添加回调接口
 		 */
-		void ScanBaseDir();
-
-        /**
-		 * 扫描出需要优先扫描的目录
-		 */
-        void ScanPriorityDir();
+		void SetScanTargetCallback(ScanTargetCallback callback);
 
 		/**
 		 * 根据规则扫描出主要的目标类型存储目录
 		 *
 		 * @param baseDirs : in, 扫描的目标目录列表
 		 * @param targetDir : out, 扫描到的目标类型存储目录列表
+		 * @param priority : in, 是否是扫描优先目录
 		 */
-		void ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std::wstring> &targetDir, ScanTargetCallback callback);
+		void ScanTargetDir(std::vector<std::wstring>* baseDir, std::vector<std::wstring> &targetDir, BOOL priority);
+
+	private :
+		/**
+		 * 初始化
+		 */
+		void Init();
+
+		/**
+		 * 反初始化
+		 */
+		void UnInit();
+
+		void PushBackDir(std::vector<std::wstring> &dirList, std::wstring &directory);
+
+		/**
+		 * 扫描出本地各磁盘的一级目录（根据规则过滤掉不在扫描范围内的目录）
+		 *
+		 * @param baseDirs : out, 扫描到的一级目录列表
+		 */
+		void ScanBaseDir();
 
     public :
         std::vector<std::wstring> m_BaseDirs;
         std::vector<std::wstring> m_PriorityDirs;
 
     private :
-        INT m_ScanDirs;
+		INT m_ScanDirs;
+		ScanTargetCallback m_ScanTargetCallback;
+		std::vector<std::wstring> m_IgnoreDirs;
     };
 }
 
