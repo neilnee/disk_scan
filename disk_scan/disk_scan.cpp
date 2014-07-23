4b0dea62f4ba0bc229b0699a9723cda3290d2048
Neil Nee <neilnee324@gmail.com>
2014-07-23 15:43:17 +0800

    开始实现管道通信

--- disk_scan/disk_scan.cpp
+++ disk_scan/disk_scan.cpp
@@ -1,6 +1,12 @@
 #include "stdafx.h"
 #include "disk_scan_tool.h"
 
+#define PIPE_BUF_SIZE 512
+
+BOOL m_Connected = FALSE;
+HANDLE m_Pipe = INVALID_HANDLE_VALUE;
+LPTSTR m_PipeName = TEXT("\\\\.\\pipe\\xlspace_disk_scan_pipe");
+
 void ScanTargetCallback(INT event, INT scan, std::wstring directory) {
     
 }
@@ -18,5 +24,33 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     //scanner->SetScanTargetCallback(ScanTargetCallback);
     //scanner->ScanTargetDir(&scanner->m_PriorityDirs, picDirs, TRUE);
     //scanner->ScanTargetDir(&scanner->m_BaseDirs, picDirs, FALSE);
+
+    for(;;) {
+        m_Pipe = CreateNamedPipe(
+            m_PipeName,
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE |
+            PIPE_READMODE_MESSAGE |
+            PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES,
+            PIPE_BUF_SIZE,
+            PIPE_BUF_SIZE,
+            0,
+            NULL);
+
+        if (m_Pipe == INVALID_HANDLE_VALUE) {
+            return -1;
+        }
+
+        m_Connected = ConnectNamedPipe(m_Pipe, NULL) ? 
+            TRUE : (GetLastError()==ERROR_PIPE_CONNECTED);
+
+        if (m_Connected) {
+
+        } else {
+            CloseHandle(m_Pipe);
+        }
+    }
+
     return 0;
 }
