7944fdc49b7322e2dfac70956800e3ba0d9d2bb7
Neil Nee <neilnee324@gmail.com>
2014-07-23 14:35:59 +0800

    增加客户端工程

--- disk_scan/disk_scan.cpp
+++ disk_scan/disk_scan.cpp
@@ -1,10 +1,8 @@
 #include "stdafx.h"
 #include "disk_scan_tool.h"
 
-INT m_Count = 0;
-
-void callback(INT event, INT scan, std::wstring directory) {
-    m_Count = scan;
+void ScanTargetCallback(INT event, INT scan, std::wstring directory) {
+    
 }
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
@@ -15,10 +13,10 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // 持久化监控目录
     // 持久化的文件信息扫描接口
     // 跨进程的通信
-    xl_ds_api::CScanner* scanner = new xl_ds_api::CScanner();
-    std::vector<std::wstring> picDirs;
-    scanner->SetScanTargetCallback(callback);
-    scanner->ScanTargetDir(&scanner->m_PriorityDirs, picDirs, TRUE);
-    scanner->ScanTargetDir(&scanner->m_BaseDirs, picDirs, FALSE);
+    //xl_ds_api::CScanner* scanner = new xl_ds_api::CScanner();
+    //std::vector<std::wstring> picDirs;
+    //scanner->SetScanTargetCallback(ScanTargetCallback);
+    //scanner->ScanTargetDir(&scanner->m_PriorityDirs, picDirs, TRUE);
+    //scanner->ScanTargetDir(&scanner->m_BaseDirs, picDirs, FALSE);
     return 0;
 }
